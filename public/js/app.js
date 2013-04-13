07ff16e3e02ae63e774c2eddf764dc8d77ced87c
acostes <arnaud.costes@gmail.com>
2013-04-13 13:06:48 +0200

    Stats using highcharts

--- public/js/app.js
+++ public/js/app.js
@@ -3,19 +3,114 @@
 /* App Module */
 
 angular.module('redistats', []).
-
-    directive('stats', function() {
-        return {
-            restrict: 'E',
-            transclude: true,
-            scope: { id: '@' },
-            controller: function($scope, $element) {
-
+    
+    directive('pie', function() {
+        return {
+            restrict: 'C',
+            replace: true,
+            scope: {
+                items: '=',
             },
-            template:
-                '<canvas id="{{stats.id}}" width="400" height="400"></canvas>',
-            replace: true
-        };
+            link: function (scope, element, attrs) {
+                var chart = new Highcharts.Chart({
+                    colors: ['#3E67AD', '#7AA6FF', '#58BCA5', '#7EBA6A', '#B57C7D', '#B2866E', '#9FAF8B'],
+                    chart: {
+                        renderTo: attrs.id,
+                    },
+                    title: {
+                      text: attrs.title
+                    },
+                    tooltip: {
+                        pointFormat: '<b>{point.y}</b>',
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true
+                            },
+                            showInLegend: true
+                        }
+                    },
+                    legend: {
+                        layout: 'horizontal',
+                        floating: false,
+                        borderRadius: 0,
+                        borderWidth: 0
+                    },
+                    series: [{
+                        type: 'pie',
+                        data: scope.items
+                    }]
+                });
+                scope.$watch("items", function (newValue) {
+                    chart.series[0].setData(newValue, true);
+                }, true);
+            }
+        }
+    }).
+
+    directive('bar', function() {
+        return {
+            restrict: 'C',
+            replace: true,
+            scope: {
+                items: '=',
+                categories: '=',
+            },
+            link: function (scope, element, attrs) {
+                var chart = new Highcharts.Chart({
+                    colors: ['#9FAF8B', '#7AA6FF', '#58BCA5', '#7EBA6A', '#B57C7D', '#B2866E'],
+                    chart: {
+                        renderTo: attrs.id,
+                        type: 'bar'
+                    },
+                    title: {
+                      text: attrs.title
+                    },
+                    xAxis: {
+                        categories: scope.categories,
+                        title: {
+                            text: null
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: null,
+                        },
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'top',
+                        x: -100,
+                        y: 100,
+                        floating: true,
+                        borderWidth: 1,
+                        backgroundColor: '#FFFFFF',
+                        shadow: true
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.y}</b>',
+                    },
+                    series: scope.items
+                });
+
+                scope.$watch("categories", function (newValue) {
+                    chart.xAxis[0].setCategories(newValue);
+                }, true);
+                
+                scope.$watch("items", function (newValue) {
+                    for (var i in newValue) {
+                        if (!chart.series[i]) {
+                           chart.addSeries(newValue[i]); 
+                       }                        
+                    }
+                }, true);
+
+            }
+        }
     }).
 
     directive('tabs', function() {
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -16,7 +16,50 @@ function MonitorInfoCtrl($scope, $routeParams, $http) {
     $scope.monitorId = $routeParams.monitorId;
     $http.get('api/info/' + $scope.monitorId).success(function(data) {
         $scope.monitorInfo = data;
-    });
+        $scope.error = data.error;
+        if (!data.error) {
+            $scope.memoryValues = [
+                ['memory', parseInt($scope.monitorInfo.Memory.used_memory)],
+                ['memory_lua', parseInt($scope.monitorInfo.Memory.used_memory_lua)],
+                ['memory_peak', parseInt($scope.monitorInfo.Memory.used_memory_peak)],
+                ['memory_rss', parseInt($scope.monitorInfo.Memory.used_memory_rss)]
+            ]
+
+            $scope.stats = [
+                ['connections_received', parseInt($scope.monitorInfo.Stats.total_connections_received)],
+                ['commands_processed', parseInt($scope.monitorInfo.Stats.total_commands_processed)]
+            ]
+
+            $scope.cmd = new Object();
+            $scope.cmd.categories = new Object();
+            $scope.cmd.values = new Array();
+            var index = 0;
+            for (var cmd in $scope.monitorInfo.Commandstats) {
+                var info = $scope.monitorInfo.Commandstats[cmd];
+                var command = cmd.split('_');
+                $scope.cmd.categories[index] = command[1];
+
+                var value = info.match(/([0-9]+(.[0-9]+)?)+/g);
+                var name = info.match(/([a-z](_[a-z])?)+/g);
+
+                for (var i = 0; i < value.length; i++) {
+                    if (!$scope.cmd.values[i]) {
+                        $scope.cmd.values[i] = new Object();
+                    }
+                        
+                    if (!$scope.cmd.values[i].data) {
+                        $scope.cmd.values[i].data = new Array();
+                    }
+
+                    $scope.cmd.values[i].data.push(parseFloat(value[i]));
+                    $scope.cmd.values[i].name = name[i];
+                }
+
+                index++;
+            }
+        }
+
+    });    
 }
 
 function MonitorStatsCtrl($scope, $routeParams, $http) {
